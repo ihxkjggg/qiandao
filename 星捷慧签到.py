baac743978ea45977e8dcbbf660c0ece397b6766
ihxkjggg <69021797+ihxkjggg@users.noreply.github.com>
2024-02-28 15:23:55 +0800

    Update R3.py

--- 星捷慧签到.py
+++ 星捷慧签到.py
@@ -1,35 +1,15 @@
-import base64
-import requests, urllib3
-import time
+import os.path
 
-urllib3.disable_warnings()
-# 请求获取cooks
-url1='http://yqjd.meiri100.cn/login/index'
-hearer={
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-"Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-"Accept-Encoding": "gzip, deflate",
-"Host": "yqjd.meiri100.cn",
-"Connection": "Keep-Alive",
-"Upgrade-Insecure-Requests":"1"
-}
-resp_0 = requests.get(url=url1,headers=hearer, verify=False)
+import pandas as pd
+import sqlite3
+import datetime
+import matplotlib.pyplot as plt
 
-
-# 拿到cooks在准备登录
-hearer['Referer']='http://yqjd.meiri100.cn/login/index'
-hearer['Cookie']=resp_0.headers['Set-Cookie']
-data={"username":"1518443","password":"yxq199"}
-url2="http://yqjd.meiri100.cn/login/login_ok"
-resp_0 = requests.post(url=url2,headers=hearer, verify=False,data=data)
-
-
-# 登录成功跳转
-timestamp = int(time.time())
-url3=f"http://yqjd.meiri100.cn/taskIssue/index?_={timestamp}"
-resp_0 = requests.get(url=url3,headers=hearer, verify=False)
-url4="http://yqjd.meiri100.cn/grade/signin_page"
-resp_0 = requests.get(url=url4,headers=hearer, verify=False)
-
-print(resp_0)
+now = datetime.datetime.today().strftime('%Y/%m/%d')
+conn = sqlite3.connect('database.db')
+df = pd.read_excel('D:\R3拦截索赔\R3JETTA拦截2024.xlsm',sheet_name='2024Jetta数据')
+df = df.drop_duplicates(subset='索赔单号')
+df = df[df['R3处理日期']==now]
+# df = df[df['R3处理日期']>"2024-02-21"]
+df.to_sql('R3', conn, index=False,if_exists='append')
+conn.close()
