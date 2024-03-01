96c0ae7f4067601e8b2dd16e9b8f07708fbd0183
ihxkjggg <69021797+ihxkjggg@users.noreply.github.com>
2024-03-01 15:38:13 +0800

    Update R3shujugengxin.py

--- R3shujugengxin.py
+++ R3shujugengxin.py
@@ -3,14 +3,14 @@ import os.path
 import pandas as pd
 import sqlite3
 import datetime
-import matplotlib.pyplot as plt
 
-now = datetime.datetime.today().strftime('%Y/%m/%d')
-conn = sqlite3.connect('database.db')
-df = pd.read_excel('D:\R3拦截索赔\R3JETTA拦截2024.xlsm',sheet_name='2024Jetta数据')
-df = df.drop_duplicates(subset='索赔单号')
-df = df[df['R3处理日期']==now]
-# df = df[df['R3处理日期']>"2024-02-21"]
-df.to_sql('R3', conn, index=False,if_exists='append')
-conn.close()
+
+# now = datetime.datetime.today().strftime('%Y/%m/%d')
+# conn = sqlite3.connect('database.db')
+# df = pd.read_excel('D:\R3拦截索赔\R3JETTA拦截2024.xlsm',sheet_name='2024Jetta数据')
+# df = df.drop_duplicates(subset='索赔单号')
+# df = df[df['R3处理日期']==now]
+# # df = df[df['R3处理日期']>"2024-02-21"]
+# df.to_sql('R3', conn, index=False,if_exists='append')
+# conn.close()
 print("OK")
